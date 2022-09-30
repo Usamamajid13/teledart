266a9cb52998038ae8fdbbd363f0ea20409581da
chaudhryusama <usamamajid13@gmail.com>
2022-09-30 18:16:24 +0500

    conversion to binance applied.

---
+++ lib/home_screen.dart
@@ -0,0 +1,173 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var telePairController = TextEditingController();
+  var teleStopLossController = TextEditingController();
+  var teleTakeProfitController = TextEditingController();
+  var teleConversionRateController = TextEditingController();
+  @override
+  void initState() {
+    getList();
+    super.initState();
+  }
+
+  getList() {
+    Timer.periodic(const Duration(seconds: 5), (timer) {
+      if (message != null) {
+        LineSplitter ls = const LineSplitter();
+        List<String> masForUsing = ls.convert(message);
+        if (kDebugMode) {
+          print(masForUsing[0].replaceAll("Pair: ", ""));
+        }
+        telePairController.text = masForUsing[0].replaceAll("Pair: ", "");
+        teleStopLossController.text = masForUsing[1].replaceAll("SL: ", "");
+        teleTakeProfitController.text = masForUsing[2].replaceAll("TP: ", "");
+        teleConversionRateController.text =
+            masForUsing[3].replaceAll("Conversion Rate: ", "");
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.width * 0.90,
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 50,
+              child: TextField(
+                controller: telePairController,
+                decoration: const InputDecoration(
+                  labelText: "Pair",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                  ), //normal border
+                  //focused border
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 50,
+              child: TextField(
+                controller: teleStopLossController,
+                decoration: const InputDecoration(
+                  labelText: "Stop Loss",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                  ), //normal border
+                  //focused border
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 50,
+              child: TextField(
+                controller: teleTakeProfitController,
+                decoration: const InputDecoration(
+                  labelText: "Take Profit",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                  ), //normal border
+                  //focused border
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 50,
+              child: TextField(
+                controller: teleConversionRateController,
+                decoration: const InputDecoration(
+                  labelText: "Conversion Rate",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                  ), //normal border
+                  //focused border
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Center(
+              child: Text(
+                "Sent To Binance",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
+import 'package:teledart_example1/constants.dart';
+import 'home_screen.dart';
 
 Future<void> main() async {
+  runApp(const MyApp());
   final username =
-      (await Telegram('5634428377:AAHtZc4wLjLDBxF_ncBs7NapjD-fkerBFTU')
-              .getMe()
-              .catchError((e) {
-    if (kDebugMode) {
-      print(e);
-    }
-  }))
+      (await Telegram('5634428377:AAHtZc4wLjLDBxF_ncBs7NapjD-fkerBFTU').getMe())
           .username;
-  if (kDebugMode) {
-    print(username);
-  }
+
   var teledart = TeleDart(
       '5634428377:AAHtZc4wLjLDBxF_ncBs7NapjD-fkerBFTU', Event(username!));
-  if (kDebugMode) {
-    print(teledart);
-  }
 
   teledart.start();
-
+  teledart.onChannelPost().listen((event) {
+    message = event.text;
+  });
   teledart
       .onMessage(entityType: 'bot_command', keyword: 'start')
-      .listen((message) => teledart.sendMessage(message.chat.id, 'I am Usama'));
+      .listen((messages) => message = messages.text.toString());
   teledart
       .onMessage(entityType: 'bot_command', keyword: 'javed')
-      .listen((message) {
-    teledart.sendMessage(message.chat.id, 'Talking about Javed Rafique');
-    if (kDebugMode) {
-      print(message.text.toString());
-    }
+      .listen((messages) {
+    message = messages.text.toString();
   });
 
   teledart
       .onMessage(entityType: 'bot_command', keyword: 'info')
-      .listen((message) {
-    teledart.sendMessage(message.chat.id, 'I am usama from Dejure.');
-    if (kDebugMode) {
-      print(message.text.toString());
-    }
+      .listen((messages) {
+    message = messages.text.toString();
   });
 
-  teledart.onCommand('USDTBTC').listen((message) {
-    message.reply('hello');
+  teledart.onCommand('USDTBTC').listen((messages) {
+    message = messages.text.toString();
   });
 
-  teledart.onMessage().listen((message) {
-    teledart.sendMessage(message.chat.id, "hi Usama");
-    if (kDebugMode) {
-      print(message.text);
-    }
+  teledart.onMessage().listen((messages) {
+    message = messages.text.toString();
   });
   teledart.onBold().listen((event) {
-    if (kDebugMode) {
-      print(event.text);
-    }
+    message = event.text;
   });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }
